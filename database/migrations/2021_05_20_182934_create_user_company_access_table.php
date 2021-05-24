178d280faa8ad41992aa23f53ddfcb856c3de722
Jovana <jovana@2amigos.us>
2021-05-24 14:45:27 +0200

    SIM-63 Migrations and models for users and companies and related entities

---
+++ database/migrations/2021_05_20_182934_create_user_company_access_table.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the 2amigos/simplify
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use App\Enums\Status;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserCompanyAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_company_access', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('company_id')->constrained('companies');
+
+            $table->primary(['user_id', 'company_id']);
+
+            $table->tinyInteger('status')->default(Status::Active);
+            $table->tinyInteger('role');
+            $table->json('permissions')->nullable();
+            $table->json('meta')->nullable();
+
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_company_access');
+    }
+}
