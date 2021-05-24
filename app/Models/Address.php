9cceb6bb4ce1abd7cac1295b88cc433907a0af0a
Jovana <jovana@2amigos.us>
2021-05-24 14:46:04 +0200

    SIM-63 Migrations and models for users and companies and related entities

---
+++ app/Models/Address.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the 2amigos/simplify
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class Address.
+ *
+ * @property int $id
+ * @property int $company_id
+ * @property string $street
+ * @property string $city
+ * @property string $state
+ * @property string $country
+ * @property string $zip_code
+ * @property string $mail_code
+ * @property string $timezone
+ * @property Carbon $created_at
+ * @property Carbon|null $updated_at
+ */
+class Address extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'company_id',
+        'street',
+        'city',
+        'state',
+        'country',
+        'zip_code',
+        'mail_code',
+        'timezone',
+    ];
+
+    /**
+     * The attributes that should be cast to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class, 'company_id');
+    }
+}
