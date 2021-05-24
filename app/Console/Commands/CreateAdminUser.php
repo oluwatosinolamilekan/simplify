898ec1dc8459b64997cb79d87101b0c888006201
Jovana <jovana@2amigos.us>
2021-05-24 15:20:28 +0200

    SIM-13 Implement console command to create admin user

--- app/Console/Commands/CreateAdminUser.php
+++ app/Console/Commands/CreateAdminUser.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Enums\Role;
 use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class CreateAdminUser extends Command
 {
@@ -44,15 +45,35 @@ class CreateAdminUser extends Command
         $attributes['first_name'] = $this->ask('Enter first name');
         $attributes['last_name'] = $this->ask('Enter last name');
         $attributes['email'] = $this->ask('Enter email');
-        $attributes['password'] = $this->ask('Enter password');
+        $attributes['password'] = $this->ask('Enter password (leave blank for random generated)', Str::random(10));
 
-        $attributes['role'] = Role::Administrator;
+        $role = Role::fromValue(Role::Administrator);
+        $attributes['role'] = $role->value;
 
-        if (!User::create($attributes)) {
-            $this->error('Something went wrong while trying to create user!');
+        if (User::where('email', $attributes['email'])->exists()) {
+            $this->error('User with given email already exists!');
+            return 0;
         }
 
-        $this->info('User successfully created!');
+        // Confirm
+        $summary = "Last name: {$attributes['last_name']} | First name: {$attributes['first_name']} | Email: {$attributes['email']} | Password: {$attributes['password']} | Role: {$role->key}";
+
+        $this->line('Please review user account details');
+        $this->line($summary);
+
+        if ($this->confirm('Do you wish to continue?', true))
+        {
+
+
+            if (!User::create($attributes)) {
+                $this->error('Something went wrong while trying to create user!');
+                return 0;
+            }
+
+            $this->info('User successfully created!');
+        }
+
+        $this->info('All done.');
 
         return 0;
     }
