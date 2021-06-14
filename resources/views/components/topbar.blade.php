71d74c3a8a5c748631953823339c8a7a49025b56
sofia <sofia@2amigos.us>
2021-06-14 16:13:07 +0400

    Make working topbar search, notifications and profile icons

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -4,92 +4,92 @@
     <div class="-intro-x breadcrumb mr-auto hidden sm:flex"> <a href="" class="breadcrumb--active">Dashboard</a> </div>
     <!-- END: Breadcrumb -->
     <!-- BEGIN: Search -->
-    {{--<div class="intro-x relative mr-3 sm:mr-6">--}}
-        {{--<div class="search hidden sm:block">--}}
-            {{--<input type="text" class="search__input form-control border-transparent placeholder-theme-13 focus:ring-theme-18 focus:border-theme-18" placeholder="Search...">--}}
-            {{--<i data-feather="search" class="search__icon dark:text-gray-300"></i>--}}
-        {{--</div>--}}
-        {{--<a class="notification sm:hidden" href=""> <i data-feather="search" class="notification__icon dark:text-gray-300"></i> </a>--}}
-        {{--<div class="search-result">--}}
-            {{--<div class="search-result__content">--}}
-                {{--<div class="search-result__content__title">Pages</div>--}}
-                {{--<div class="mb-5">--}}
-                    {{--<a href="" class="flex items-center">--}}
-                        {{--<div class="w-8 h-8 bg-theme-18 text-theme-9 flex items-center justify-center rounded-full"> <i class="w-4 h-4" data-feather="inbox"></i> </div>--}}
-                        {{--<div class="ml-3">Mail Settings</div>--}}
-                    {{--</a>--}}
-                    {{--<a href="" class="flex items-center mt-2">--}}
-                        {{--<div class="w-8 h-8 bg-theme-17 text-theme-11 flex items-center justify-center rounded-full"> <i class="w-4 h-4" data-feather="users"></i> </div>--}}
-                        {{--<div class="ml-3">Users & Permissions</div>--}}
-                    {{--</a>--}}
-                    {{--<a href="" class="flex items-center mt-2">--}}
-                        {{--<div class="w-8 h-8 bg-theme-14 text-theme-10 flex items-center justify-center rounded-full"> <i class="w-4 h-4" data-feather="credit-card"></i> </div>--}}
-                        {{--<div class="ml-3">Transactions Report</div>--}}
-                    {{--</a>--}}
-                {{--</div>--}}
-                {{--<div class="search-result__content__title">Users</div>--}}
-                {{--<div class="mb-5">--}}
-                    {{--<a href="" class="flex items-center mt-2">--}}
-                        {{--<div class="w-8 h-8 image-fit">--}}
-                            {{--<img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-5.jpg">--}}
-                        {{--</div>--}}
-                        {{--<div class="ml-3">Al Pacino</div>--}}
-                        {{--<div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">alpacino@left4code.com</div>--}}
-                    {{--</a>--}}
-                    {{--<a href="" class="flex items-center mt-2">--}}
-                        {{--<div class="w-8 h-8 image-fit">--}}
-                            {{--<img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-15.jpg">--}}
-                        {{--</div>--}}
-                        {{--<div class="ml-3">Denzel Washington</div>--}}
-                        {{--<div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">denzelwashington@left4code.com</div>--}}
-                    {{--</a>--}}
-                    {{--<a href="" class="flex items-center mt-2">--}}
-                        {{--<div class="w-8 h-8 image-fit">--}}
-                            {{--<img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-12.jpg">--}}
-                        {{--</div>--}}
-                        {{--<div class="ml-3">Kevin Spacey</div>--}}
-                        {{--<div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">kevinspacey@left4code.com</div>--}}
-                    {{--</a>--}}
-                    {{--<a href="" class="flex items-center mt-2">--}}
-                        {{--<div class="w-8 h-8 image-fit">--}}
-                            {{--<img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-7.jpg">--}}
-                        {{--</div>--}}
-                        {{--<div class="ml-3">Nicolas Cage</div>--}}
-                        {{--<div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">nicolascage@left4code.com</div>--}}
-                    {{--</a>--}}
-                {{--</div>--}}
-                {{--<div class="search-result__content__title">Products</div>--}}
-                {{--<a href="" class="flex items-center mt-2">--}}
-                    {{--<div class="w-8 h-8 image-fit">--}}
-                        {{--<img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/preview-15.jpg">--}}
-                    {{--</div>--}}
-                    {{--<div class="ml-3">Samsung Galaxy S20 Ultra</div>--}}
-                    {{--<div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Smartphone &amp; Tablet</div>--}}
-                {{--</a>--}}
-                {{--<a href="" class="flex items-center mt-2">--}}
-                    {{--<div class="w-8 h-8 image-fit">--}}
-                        {{--<img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/preview-7.jpg">--}}
-                    {{--</div>--}}
-                    {{--<div class="ml-3">Samsung Galaxy S20 Ultra</div>--}}
-                    {{--<div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Smartphone &amp; Tablet</div>--}}
-                {{--</a>--}}
-                {{--<a href="" class="flex items-center mt-2">--}}
-                    {{--<div class="w-8 h-8 image-fit">--}}
-                        {{--<img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/preview-10.jpg">--}}
-                    {{--</div>--}}
-                    {{--<div class="ml-3">Sony Master Series A9G</div>--}}
-                    {{--<div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Electronic</div>--}}
-                {{--</a>--}}
-                {{--<a href="" class="flex items-center mt-2">--}}
-                    {{--<div class="w-8 h-8 image-fit">--}}
-                        {{--<img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/preview-1.jpg">--}}
-                    {{--</div>--}}
-                    {{--<div class="ml-3">Samsung Q90 QLED TV</div>--}}
-                    {{--<div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Electronic</div>--}}
-                {{--</a>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
+    <div class="intro-x relative mr-3 sm:mr-6">
+        <div class="search hidden sm:block">
+            <input type="text" class="search__input form-control border-transparent placeholder-theme-13 focus:ring-theme-18 focus:border-theme-18" placeholder="Search...">
+            <i data-feather="search" class="search__icon dark:text-gray-300"></i>
+        </div>
+        <a class="notification sm:hidden" href=""> <i data-feather="search" class="notification__icon dark:text-gray-300"></i> </a>
+        <div class="search-result">
+            <div class="search-result__content">
+                <div class="search-result__content__title">Pages</div>
+                <div class="mb-5">
+                    <a href="" class="flex items-center">
+                        <div class="w-8 h-8 bg-theme-18 text-theme-9 flex items-center justify-center rounded-full"> <i class="w-4 h-4" data-feather="inbox"></i> </div>
+                        <div class="ml-3">Mail Settings</div>
+                    </a>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 bg-theme-17 text-theme-11 flex items-center justify-center rounded-full"> <i class="w-4 h-4" data-feather="users"></i> </div>
+                        <div class="ml-3">Users & Permissions</div>
+                    </a>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 bg-theme-14 text-theme-10 flex items-center justify-center rounded-full"> <i class="w-4 h-4" data-feather="credit-card"></i> </div>
+                        <div class="ml-3">Transactions Report</div>
+                    </a>
+                </div>
+                <div class="search-result__content__title">Users</div>
+                <div class="mb-5">
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 image-fit">
+                            <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-5.jpg">
+                        </div>
+                        <div class="ml-3">Al Pacino</div>
+                        <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">alpacino@left4code.com</div>
+                    </a>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 image-fit">
+                            <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-15.jpg">
+                        </div>
+                        <div class="ml-3">Denzel Washington</div>
+                        <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">denzelwashington@left4code.com</div>
+                    </a>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 image-fit">
+                            <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-12.jpg">
+                        </div>
+                        <div class="ml-3">Kevin Spacey</div>
+                        <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">kevinspacey@left4code.com</div>
+                    </a>
+                    <a href="" class="flex items-center mt-2">
+                        <div class="w-8 h-8 image-fit">
+                            <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-7.jpg">
+                        </div>
+                        <div class="ml-3">Nicolas Cage</div>
+                        <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">nicolascage@left4code.com</div>
+                    </a>
+                </div>
+                <div class="search-result__content__title">Products</div>
+                <a href="" class="flex items-center mt-2">
+                    <div class="w-8 h-8 image-fit">
+                        <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/preview-15.jpg">
+                    </div>
+                    <div class="ml-3">Samsung Galaxy S20 Ultra</div>
+                    <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Smartphone &amp; Tablet</div>
+                </a>
+                <a href="" class="flex items-center mt-2">
+                    <div class="w-8 h-8 image-fit">
+                        <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/preview-7.jpg">
+                    </div>
+                    <div class="ml-3">Samsung Galaxy S20 Ultra</div>
+                    <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Smartphone &amp; Tablet</div>
+                </a>
+                <a href="" class="flex items-center mt-2">
+                    <div class="w-8 h-8 image-fit">
+                        <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/preview-10.jpg">
+                    </div>
+                    <div class="ml-3">Sony Master Series A9G</div>
+                    <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Electronic</div>
+                </a>
+                <a href="" class="flex items-center mt-2">
+                    <div class="w-8 h-8 image-fit">
+                        <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="dist/images/preview-1.jpg">
+                    </div>
+                    <div class="ml-3">Samsung Q90 QLED TV</div>
+                    <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right">Electronic</div>
+                </a>
+            </div>
+        </div>
+    </div>
     <!-- END: Search -->
     <!-- BEGIN: Notifications -->
     <div class="intro-x dropdown mr-auto sm:mr-6">
