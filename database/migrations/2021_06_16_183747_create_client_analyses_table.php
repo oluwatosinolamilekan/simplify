e0055941b893d2f984d9384723eaef05f47ca783
Jovana <jovana@2amigos.us>
2021-06-21 20:39:11 +0200

    SIM-19 Correct migration name

--- database/migrations/2021_06_16_183747_create_client_analyses_table.php
+++ database/migrations/2021_06_16_183747_create_client_analyses_table.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientAnalysisTable extends Migration
+class CreateClientAnalysesTable extends Migration
 {
     /**
      * Run the migrations.
