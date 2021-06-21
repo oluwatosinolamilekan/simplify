7cecb4dd7ddc76844ad96a1c17b1b32a42e1ac85
Jovana <jovana@2amigos.us>
2021-06-21 20:34:59 +0200

    SIM-19 Correct migration name

--- database/migrations/2021_06_16_174219_create_payment_batches_table.php
+++ database/migrations/2021_06_16_174219_create_payment_batches_table.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentBatchTable extends Migration
+class CreatePaymentBatchesTable extends Migration
 {
     /**
      * Run the migrations.
