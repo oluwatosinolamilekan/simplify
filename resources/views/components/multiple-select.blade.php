991b5a2499a6677bee2a5d2de599f70b8bc0a22a
sofia <sofia@2amigos.us>
2021-07-01 18:10:37 +0400

    Created Tag multiselect component

---
+++ resources/views/components/multiple-select.blade.php
@@ -0,0 +1,13 @@
+<!-- BEGIN: Multi Select -->
+<div id="multi-select" class="p-5">
+    <div class="preview">
+        <select data-placeholder="Select your favorite actors" data-search="true" class="tail-select w-full" multiple>
+            <option value="1" selected>Leonardo DiCaprio</option>
+            <option value="2">Johnny Deep</option>
+            <option value="3" selected>Robert Downey, Jr</option>
+            <option value="4">Samuel L. Jackson</option>
+            <option value="5">Morgan Freeman</option>
+        </select>
+    </div>
+</div>
+<!-- END: Multi Select -->
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,4 +6,14 @@
     <div class="mt-8">
         <h2 class="text-lg">Welcome Addio application!</h2>
     </div>
+
+
+    <div class="intro-y box mt-5">
+        <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
+            <h2 class="font-medium text-base mr-auto">
+                Multi Select
+            </h2>
+        </div>
+        <x-multiple-select/>
+    </div>
 </x-app-layout>
