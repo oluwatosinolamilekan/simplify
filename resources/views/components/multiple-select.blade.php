183c29c0ad0256fd33813d57218ad7d2fa7f6e0f
sofia <sofia@2amigos.us>
2021-07-05 15:35:25 +0400

    Make tag component reusable

--- resources/views/components/multiple-select.blade.php
+++ resources/views/components/multiple-select.blade.php
@@ -1,12 +1,12 @@
+@props(['values'])
+
 <!-- BEGIN: Multi Select -->
 <div id="multi-select" class="p-5">
     <div class="preview">
-        <select data-placeholder="Select your favorite actors" data-search="true" class="tail-select w-full" multiple>
-            <option value="1" selected>Leonardo DiCaprio</option>
-            <option value="2">Johnny Deep</option>
-            <option value="3" selected>Robert Downey, Jr</option>
-            <option value="4">Samuel L. Jackson</option>
-            <option value="5">Morgan Freeman</option>
+        <select data-placeholder="Select option" data-search="true" class="tail-select w-full" multiple>
+            @foreach ($values as $value)
+                <option value="{{ $value->id }}" {{$value->selected ? 'selected' : ''}}>{{ $value->name }}</option>
+            @endforeach
         </select>
     </div>
 </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,14 @@
         <h2 class="text-lg">Welcome Addio application!</h2>
     </div>
 
+    <?php
+    $values = (object) array (
+        (object) array('id' => '1', 'name' => 'mek', 'selected' => 'selected'),
+        (object) array('id' => '2', 'name' => 'erku', 'selected' => ''),
+        (object) array('id' => '3', 'name' => 'ereq', 'selected' => 'selected'),
+        (object) array('id' => '4', 'name' => 'chors', 'selected' => '')
+    );
+    ?>
 
     <div class="intro-y box mt-5">
         <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
@@ -14,6 +22,6 @@
                 Multi Select
             </h2>
         </div>
-        <x-multiple-select/>
+        <x-multiple-select :values="$values"/>
     </div>
 </x-app-layout>
