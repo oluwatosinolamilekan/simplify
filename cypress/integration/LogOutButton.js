3b1058ad91a75c559a175dff7f5b4fc3691e3486
tatjana@2amigos.us <Sehovac9>
2021-07-09 16:59:00 +0200

    Aligned the code

--- cypress/integration/LogOutButton.js
+++ cypress/integration/LogOutButton.js
@@ -3,10 +3,9 @@ it('Logs out users', () => {
         cy.visit('/login');
         cy.typeLogin({email: user.email , password: 'password'});
 
-       cy.location('pathname').should('eq', '/');
-         cy.Logout(user);
+        cy.location('pathname').should('eq', '/');
+        cy.Logout(user);
         cy.go(-1)
         cy.location('pathname').should('eq', '/login');
-
     });
 });
