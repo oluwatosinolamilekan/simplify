46f06d25fc3ae4c0057ddc7c1d7b9a675e839183
tatjana@2amigos.us <Sehovac9>
2021-07-09 14:14:59 +0200

    SIM-115 Create automation tests for logout process

---
+++ cypress/integration/LogOutButton.js
@@ -0,0 +1,12 @@
+it('Logs out users', () => {
+    cy.create('App\\Models\\User').then(user => {
+        cy.visit('/login');
+        cy.typeLogin({email: user.email , password: 'password'});
+
+       cy.location('pathname').should('eq', '/');
+         cy.Logout(user);
+        cy.go(-1)
+        cy.location('pathname').should('eq', '/login');
+
+    });
+});
