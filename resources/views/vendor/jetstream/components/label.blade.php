c18c28cfd16dcd4c52a33a3060b2578c5aa79cdd
Jovana <jovana@2amigos.us>
2021-07-13 14:36:46 +0200

    SIM-98 Input and label components

--- resources/views/vendor/jetstream/components/label.blade.php
+++ resources/views/vendor/jetstream/components/label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'inline-block font-medium text-sm text-gray-700 sm:inline-block w-1/2 mt-3 text-wrap']) }}>
     {{ $value ?? $slot }}
 </label>
