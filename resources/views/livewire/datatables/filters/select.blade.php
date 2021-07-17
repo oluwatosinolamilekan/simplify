7ecea0993a5f134e28bd8b2ac18a89716946d190
sofia <sofia@2amigos.us>
2021-07-17 20:17:46 +0400

    Table component styling

--- resources/views/livewire/datatables/filters/select.blade.php
+++ resources/views/livewire/datatables/filters/select.blade.php
@@ -1,31 +1,31 @@
-<div x-data class="flex flex-col">
+<div x-data class="flex flex-col mr-2.5">
     <div class="flex">
-        <select
+        <x-select
             x-ref="select"
             name="{{ $name }}"
-            class="m-1 text-sm leading-4 flex-grow form-select"
             wire:input="applyFilter('select', '{{ $index }}', $event.target.value)"
             x-on:input="$refs.select.value=''"
+            class="text-gray-500 h-8 mt-2.5"
         >
-            <option value=""></option>
+            <option value="">Select..</option>
             @foreach($options as $value => $label)
-            @if(is_object($label))
-            <option value="{{ $label->id }}">{{ $label->name }}</option>
-            @elseif(is_array($label))
-            <option value="{{ $label['id'] }}">{{ $label['name'] }}</option>
-            @elseif(is_numeric($value))
-            <option value="{{ $label }}">{{ $label }}</option>
-            @else
-            <option value="{{ $value }}">{{ $label }}</option>
-            @endif
+                @if(is_object($label))
+                    <option value="{{ $label->id }}" class="text-gray-800">{{ $label->name }}</option>
+                @elseif(is_array($label))
+                    <option value="{{ $label['id'] }}" class="text-gray-800">{{ $label['name'] }}</option>
+                @elseif(is_numeric($value))
+                    <option value="{{ $label }}" class="text-gray-800">{{ $label }}</option>
+                @else
+                    <option value="{{ $value }}" class="text-gray-800">{{ $label }}</option>
+                @endif
             @endforeach
-        </select>
+        </x-select>
     </div>
 
     <div class="flex flex-wrap max-w-48 space-x-1">
         @foreach($this->activeSelectFilters[$index] ?? [] as $key => $value)
         <button wire:click="removeFilter('select', '{{ $index }}', '{{ $key }}')" x-on:click="$refs.select.value=''"
-            class="m-1 pl-1 flex items-center uppercase tracking-wide bg-gray-300 text-white hover:bg-red-600 rounded-full focus:outline-none text-xs space-x-1">
+            class="m-1 pl-1 flex items-center uppercase tracking-wide bg-gray-300 text-white hover:bg-red-400 rounded-full focus:outline-none text-xs space-x-1">
             <span>{{ $this->getDisplayValue($index, $value) }}</span>
             <x-icons.x-circle />
         </button>
