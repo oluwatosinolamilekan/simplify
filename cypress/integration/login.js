2f2334f0ba96a035ddb7c9af7a4a6e78340a0d9c
tatjana@2amigos.us <Sehovac9>
2021-07-19 18:31:45 +0200

    Added login scenarios

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,9 +1,31 @@
 it('Logs in users', () => {
     cy.create('App\\Models\\User').then(user => {
+        let correctEmail = user.email;
+        let correctPassword = 'password';
+        let incorrectEmail = 'random@email.com';
+        let incorrectPassword = 'random';
         cy.visit('/login');
 
-        cy.userLogin(user);
+        cy.userLogin({email: '    ', password: '    '})
+        cy.location('pathname').should('eq', '/login');
 
-        cy.contains(user.first_name);
+        cy.userLogin({email: correctEmail , password: '    '})
+        cy.location('pathname').should('eq', '/login');
+        cy.get('input[name="email"]').clear();
+
+        cy.userLogin({email: incorrectEmail, password: incorrectPassword})
+        cy.get('input[name="email"]').clear();
+        cy.location('pathname').should('eq', '/login');
+
+        cy.userLogin({email: incorrectEmail , password: correctPassword})
+        cy.location('pathname').should('eq', '/login');
+        cy.get('input[name="email"]').clear();
+
+        cy.userLogin({email: correctEmail , password: incorrectPassword})
+        cy.location('pathname').should('eq', '/login');
+        cy.get('input[name="email"]').clear();
+
+        cy.userLogin({email: correctEmail , password: correctPassword})
+        cy.location('pathname').should('eq', '/');
     });
 });
