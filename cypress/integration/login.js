150d63e7e01ca0cb8a514e5a045051159a1cb4d9
tatjana@2amigos.us <Sehovac9>
2021-07-21 13:36:59 +0200

    Added error messages

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -6,23 +6,38 @@ it('Logs in users', () => {
         let incorrectPassword = 'random';
         cy.visit('/login');
 
-        cy.userLogin({email: '    ', password: '    '})
-        cy.location('pathname').should('eq', '/login');
+        cy.get('input[name="email"]').clear();
+        cy.get('input[name="password"]').clear();
+        cy.get('button[type="submit"]').click();
+        cy.get('input:invalid').should('have.length', 2)
+        cy.get('input[name="email"]').then(($input) => {
+            expect($input[0].validationMessage).to.eq('Please fill out this field.')
+        })
+        cy.location('pathname').should('eq','/login');
 
-        cy.userLogin({email: correctEmail , password: '    '})
-        cy.location('pathname').should('eq', '/login');
+        cy.get('input[name="email"]').type(correctEmail);
+        cy.get('button[type="submit"]').click();
+        cy.get('input:invalid').should('have.length', 1)
+        cy.get('input[name="password"]').then(($input) => {
+            expect($input[0].validationMessage).to.eq('Please fill out this field.')
+        })
+        cy.location('pathname').should('eq','/login');
+        cy.get('input[name="email"]').clear();
+
+        cy.userLogin({email: correctEmail , password: '   '})
+        cy.errorMessageLogin('The password field is required.')
         cy.get('input[name="email"]').clear();
 
         cy.userLogin({email: incorrectEmail, password: incorrectPassword})
+        cy.errorMessageLogin('These credentials do not match our records.')
         cy.get('input[name="email"]').clear();
-        cy.location('pathname').should('eq', '/login');
 
         cy.userLogin({email: incorrectEmail , password: correctPassword})
-        cy.location('pathname').should('eq', '/login');
+        cy.errorMessageLogin('These credentials do not match our records.')
         cy.get('input[name="email"]').clear();
 
         cy.userLogin({email: correctEmail , password: incorrectPassword})
-        cy.location('pathname').should('eq', '/login');
+        cy.errorMessageLogin('These credentials do not match our records.')
         cy.get('input[name="email"]').clear();
 
         cy.userLogin({email: correctEmail , password: correctPassword})
