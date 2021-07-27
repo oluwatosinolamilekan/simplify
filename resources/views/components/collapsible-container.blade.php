db57224530c01183a7da77ed0c450ee0444dcfcb
oluwatosinolamilekan <oluwatosinolamilekan@gmail.com>
2021-07-27 14:19:42 +0100

    remove the addition and cancel sign on the expanding button

--- resources/views/components/collapsible-container.blade.php
+++ resources/views/components/collapsible-container.blade.php
@@ -5,7 +5,7 @@
                                                 }">
     <div class="px-4 py-5 text-right sm:p-6 shadow sm:rounded-md">
         <x-success-anchor @click="toggle()">
-            <span  x-text="!open ? '+ Add' : '- Cancel' ">
+            <span  x-text="!open ? 'Add' : 'Cancel' ">
             </span>
         </x-success-anchor>
     </div>
