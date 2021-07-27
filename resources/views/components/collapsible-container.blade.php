03fe0b1d5bca8fc698adfa217fbd0c2ae7029ec8
oluwatosinolamilekan <oluwatosinolamilekan@gmail.com>
2021-07-27 14:59:46 +0100

    WIP

--- resources/views/components/collapsible-container.blade.php
+++ resources/views/components/collapsible-container.blade.php
@@ -1,8 +1,8 @@
 <div class="mt-5 md:mt-0 md:col-span-2"  x-data="{
-                                                  open: false,
-                                                  get isOpen() { return this.open },
-                                                  toggle() { this.open = ! this.open },
-                                                }">
+          open: false,
+          get isOpen() { return this.open },
+          toggle() { this.open = ! this.open },
+        }">
     <div class="px-4 py-5 text-right sm:p-6 shadow sm:rounded-md">
         <x-success-anchor @click="toggle()">
             <span  x-text="!open ? 'Add' : 'Cancel' ">
