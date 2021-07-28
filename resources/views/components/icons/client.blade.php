2d07c2206356b123804be0307002221eceb11597
oluwatosinolamilekan <oluwatosinolamilekan@gmail.com>
2021-07-28 11:34:27 +0100

    all icons are active

--- resources/views/components/icons/client.blade.php
+++ resources/views/components/icons/client.blade.php
@@ -1,4 +1,4 @@
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {!! $attributes->merge(['class' => '']) !!}>
     <path d="M1.0625 17.1039C1.064 14.9003 2.17094 12.8443 4.00978 11.6299C5.84862 10.4155 8.17407 10.2046 10.2014 11.0684" stroke="#718096" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M7.62501 9.08333C9.8399 9.08333 11.6354 7.28781 11.6354 5.07292C11.6354 2.85802 9.8399 1.0625 7.62501 1.0625C5.41012 1.0625 3.61459 2.85802 3.61459 5.07292C3.61459 7.28781 5.41012 9.08333 7.62501 9.08333Z" stroke="#718096" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
     <rect x="10.0556" y="14.1875" width="10.9375" height="8.02083" rx="1.75" stroke="#718096" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
