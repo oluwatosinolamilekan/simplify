0aa512263bebc3115271b85eeffc6b4cb8048188
oluwatosinolamilekan <oluwatosinolamilekan@gmail.com>
2021-07-29 00:53:23 +0100

    fixed mobile sider icons

---
+++ app/View/Components/SelectComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\View\Components;
+
+use Livewire\Component;
+
+class SelectComponent extends Component
+{
+    public function render()
+    {
+        return view('select-component');
+    }
+}
