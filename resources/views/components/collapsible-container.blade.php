50dc9d48b551681c36391e2e03277a3f715a4448
Jovana <jovana@2amigos.us>
2021-08-02 23:12:05 +0200

    Fix collapsible-container.blade.php

--- resources/views/components/collapsible-container.blade.php
+++ resources/views/components/collapsible-container.blade.php
@@ -1,11 +1,11 @@
 <div class="mt-5 md:mt-0 md:col-span-2"
      x-data="{
-          open: {{ !($collapsed ?? true) }},
+          open: {{ ($collapsed ?? true) ? 0 : 1 }},
           get isOpen() { return this.open },
           toggle() { this.open = ! this.open },
         }">
     <div class="px-4 py-5 text-right sm:p-6 shadow sm:rounded-md">
-        <span class="float-left">{{ $header ?? 'No information to show.'}}</span>
+        <span class="float-left">{{ $header ?? ''}}</span>
         <x-success-anchor @click="toggle()">
             <span x-text="!open ? 'Add' : 'Cancel' "></span>
         </x-success-anchor>
