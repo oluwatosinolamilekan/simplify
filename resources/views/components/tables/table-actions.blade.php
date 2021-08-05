b82fd5a755286227ebe66872df82821a8eb162a9
Jovana <jovana@2amigos.us>
2021-08-05 20:53:22 +0200

    SIM-170 Filter fields disappear

--- resources/views/components/tables/table-actions.blade.php
+++ resources/views/components/tables/table-actions.blade.php
@@ -1,4 +1,4 @@
-<div class="flex space-x-1 justify-around" id="{{ $id }}">
+<div class="flex space-x-1 justify-around" id="table_actions_{{ $id }}">
     @if ($view)
     <a href="{{ route($view['route'] ?? $view, $args ?? [$id]) }}" id="datatable_view_icon_{{$id}}" class="p-1 hover:bg-gray-300 dark:hover:bg-dark-2 rounded flex items-center justify-center transition">
         <x-icons.view class="view_icon"/>
